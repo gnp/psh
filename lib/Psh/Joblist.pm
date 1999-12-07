80b2dcbd0da5ab112d7791fff3dc8b25dd7ddd52
Markus Peter <warp@spin.de>
1999-12-07 22:16:53 +0000

    Fixed a bug in print_i18n Made $VERSION to update automatically in all packages Beginning of Term::ReadLine::Perl support

--- lib/Psh/Joblist.pm
+++ lib/Psh/Joblist.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION);
 
 use Psh::Job;
 
-$VERSION = '0.01';
+$VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r }; # must be all one line, for MakeMaker
 
 sub new {
 	my $class = shift;
