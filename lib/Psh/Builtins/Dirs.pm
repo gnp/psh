ef589de290bab6ff93fa7b49e72399cad1aa9474
Markus Peter <warp@spin.de>
2000-03-13 20:14:22 +0000

    The 'cd' builtin now has a (currently unlimited) history list, accessible using cd -/+/%number. The new builtin 'dirs' displays the stack

---
+++ lib/Psh/Builtins/Dirs.pm
@@ -0,0 +1,43 @@
+package Psh::Builtins::Dirs;
+use strict;
+
+use Psh::Util ':all';
+
+=item * C<dirs> [n]
+
+Prints out [the last n] entries in the cd history
+
+=cut
+
+sub bi_dirs {
+	my $max=$#Psh::Builtins::dir_stack;
+	if ($_[0] && $_[0]=~/^\d+$/) {
+		$max=$_[0]-1 if $_[0]<=$max;
+	}
+
+	for (my $i=$max; $i>=0; $i--) {
+		printf "%%%-2d ",$i;
+		
+		if ($i==$Psh::Builtins::dir_stack_pos) {
+			print " > ";
+		} else {
+			print "   ";
+		}
+		print $Psh::Builtins::dir_stack[$i]."\n";
+	}
+
+	return undef;
+}
+
+1;
+
+# Local Variables:
+# mode:perl
+# tab-width:4
+# indent-tabs-mode:t
+# c-basic-offset:4
+# perl-label-offset:0
+# perl-indent-level:4
+# cperl-indent-level:4
+# cperl-label-offset:0
+# End:
