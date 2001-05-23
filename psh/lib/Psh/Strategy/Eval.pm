1fc30ffa4c7655f303239fc5007bec402b1ad3cf
Markus Peter <warp@spin.de>
2001-05-23 19:08:59 +0000

    Preparing to switch to new strategies code

---
+++ psh/lib/Psh/Strategy/Eval.pm
@@ -0,0 +1,40 @@
+package Psh::Strategy::Eval;
+
+require Psh::Strategy;
+
+use strict;
+use vars(@ISA);
+
+@ISA=('Psh::Strategy');
+
+sub consumes {
+	return Psh::Strategy::CONSUME_TOKENS;
+}
+
+sub applies {
+	return 'perl evaluation';
+}
+
+sub execute {
+	my $todo= $$_[1];
+
+	if( $_[4]) { # we are second or later in a pipe
+		my $code;
+		$todo=~ s/\} ?([qg])\s*$/\}/;
+		my $mods= $1 || '';
+		if( $mods eq 'q' ) { # non-print mode
+			$code='while(<STDIN>) { @_= split /\s+/; '.$todo.' ; }';
+		} elsif( $mods eq 'g') { # grep mode
+			$code='while(<STDIN>) { @_= split /\s+/; print $_ if eval { '.$todo.' }; } ';
+		} else {
+			$code='while(<STDIN>) { @_= split /\s+/; '.$todo.' ; print $_ if $_; }';
+		}
+		return (sub {return Psh::PerlEval::protected_eval($code,'eval'); }, [], 0, undef);
+    } else {
+		return (sub {
+			return Psh::PerlEval::protected_eval($todo,'eval');
+		}, [], 0, undef);
+	}
+}
+
+1;
