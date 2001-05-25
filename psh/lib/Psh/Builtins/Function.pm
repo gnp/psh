fd7cfd73fc69291d209a8835fd1696d5176dc04e
Markus Peter <warp@spin.de>
2001-05-25 16:25:11 +0000

    Fixed/extended sh-like "function" support

--- psh/lib/Psh/Builtins/Function.pm
+++ psh/lib/Psh/Builtins/Function.pm
@@ -1,31 +1,24 @@
 package Psh::Builtins::Function;
-use strict;
 
-use Psh::Util ':all';
+require Psh::PerlEval;
+require Psh::Support::Functions;
 
 =item * C<function>
 
 Function tries to emulate the functionality of bash's function builtin
+
 =cut
 
 sub bi_function
 {
-	$_[0]=~/(\S+)\s*\{(.*)\}/;
+	$_[0]=~/^\s*(\S+)\s*\{(.*)\}\s*$/s;
 	my $name=$1;
 	my $def= $2;
-	Psh::PerlEval::protected_eval(qq[sub $name {Psh::evl('$def')}],'eval');
+	if ($name and $def) {
+		Psh::Support::Functions::add_function($name,$def);
+		Psh::PerlEval::protected_eval(qq[sub $name { Psh::Support::Functions::call_function($name); }], 'eval');
+	}
 	return undef;
 }
 
 1;
-
-# Local Variables:
-# mode:perl
-# tab-width:4
-# indent-tabs-mode:t
-# c-basic-offset:4
-# perl-label-offset:0
-# perl-indent-level:4
-# cperl-indent-level:4
-# cperl-label-offset:0
-# End:
