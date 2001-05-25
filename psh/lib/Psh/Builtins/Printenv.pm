d12a8f0cbb643655365235721f489ab69b60cbad
Markus Peter <warp@spin.de>
2001-05-25 21:58:41 +0000

    Added missing builtin

---
+++ psh/lib/Psh/Builtins/Printenv.pm
@@ -0,0 +1,28 @@
+package Psh::Builtins::Printenv;
+
+require Psh::Support::Env;
+require Psh::Util;
+
+=item * C<printenv NAME [NAME ...]>
+
+Displays the named environment variables
+
+=cut
+
+sub bi_printenv
+{
+	my @names= @{$_[1]};
+	if (@names) {
+		foreach (@names) {
+			Psh::Util::print_out($ENV{uc($_)}."\n");
+		}
+	} else {
+		while (my ($key,$val)= each %ENV) {
+			Psh::Util::print_out("$key=$val\n");
+		}
+	}
+	return undef;
+}
+
+
+1;
