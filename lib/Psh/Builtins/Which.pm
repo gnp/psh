af83151128bf4bb99ac8ce270a6f63a9d56b01db
Markus Peter <warp@spin.de>
2001-05-26 20:41:37 +0000

    Added a more sh-like which

--- lib/Psh/Builtins/Which.pm
+++ lib/Psh/Builtins/Which.pm
@@ -1,71 +1,24 @@
 package Psh::Builtins::Which;
 
-use Psh::Util ':all';
+require Psh::Util;
 
-=item * C<which COMMAND-LINE>
+=item * C<which command>
 
-Describe how B<psh> will execute the given COMMAND-LINE, under the
-current setting of C<$Psh::strategies>.
+Locates the command in the filesystem.
 
 =cut
 
 sub bi_which
 {
-	my $line   = shift;
-
-	print "Currently disabled.\n";
+	my $command= shift @{$_[1]};
+	if ($command) {
+		$command= Psh::Util::which($command);
+		if ($command) {
+			Psh::Util::print_out("$command\n");
+			return (1,undef);
+		}
+	}
 	return (0,undef);
-
-#  	if (!defined($line) or $line eq '') {
-#  		print_error_i18n('bi_which_no_command');
-#  		return 1;
-#  	}
-
-
-#  	print_debug("[which $line]\n");
-
-#  	my @words= Psh::Parser::std_tokenize($line);
-#  	foreach my $strat (@Psh::unparsed_strategies) {
-#  		if (!exists($Psh::strategy_which{$strat})) {
-#  			print_warning_i18n('no_such_strategy',$strat,'which');
-#  			next;
-#  		}
-
-#  		my $how = &{$Psh::strategy_which{$strat}}(\$line,\@words);
-
-#  		if ($how) {
-#  			if (ref $how eq 'ARRAY') {
-#  				$how=$how->[0]
-#  			}
-#  			print_out_i18n('evaluates_under',$line,$strat,$how);
-#  			return 0;
-#  		}
-#  	}
-
-
-#  	my @words= Psh::Parser::decompose(
-#  							'(\s+|\||;|\&\d*|[0-9&]*>|[0-9&]*<|\\|=)',
-#  							$line, undef, 1,
-#  							{"'"=>"'","\""=>"\"","{"=>"}"});
-#  	# TODO: The special rules are missing so this is not really correct
-
-#  	my $line= join ' ', @words;
-#  	foreach my $strat (@Psh::strategies) {
-#  		if (!defined($Psh::strategy_which{$strat})) {
-#  			print_warning_i18n('no_such_strategy',$strat,'which');
-#  			next;
-#  		}
-
-#  		my $how = &{$Psh::strategy_which{$strat}}(\$line,\@words);
-
-#  		if ($how) {
-#  			print_out_i18n('evaluates_under',$line,$strat,$how);
-#  			return 0;
-#  		}
-#  	}
-
-#  	print_warning_i18n('clueless',$line,'which');
-#  	return 1;
 }
 
 1;
