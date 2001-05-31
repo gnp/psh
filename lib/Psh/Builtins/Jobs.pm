4adf2e722440ae166c368fa76e8e5209efd00f91
Markus Peter <warp@spin.de>
2001-05-31 13:01:30 +0000

    Removed some unnecessary warnings Added command line options to "jobs" - Hiroo's PCompletion examples should therefore finally work completely

--- lib/Psh/Builtins/Jobs.pm
+++ lib/Psh/Builtins/Jobs.pm
@@ -3,35 +3,52 @@ package Psh::Builtins::Jobs;
 require Psh::Util;
 require Psh::Joblist;
 require Psh::OS;
+require Getopt::Std;
 
-=item * C<jobs>
+=item * C<jobs [-rs] [-p]>
 
 List the currently running jobs.
+
+Option B<-r> restricts the output to display only currently running jobs.
+Option B<-s> will only show currently stopped jobs
+
+If you specify option B<-p> only the PIDs of the processes are displayed.
 
 =cut
 
 sub bi_jobs {
+	my $line= shift;
+	local @ARGV = @{shift()};
+
 	if( ! Psh::OS::has_job_control()) {
 		Psh::Util::print_error_i18n('no_jobcontrol');
 		return (0,undef);
 	}
-
+	my $opt={};
+	Getopt::Std::getopts('prs',$opt);
 
 	my $result = '';
 	my $job;
-	my $visindex=1;
+	my $visindex=0;
 
 	Psh::Joblist::enumerate();
 
 	while( ($job=Psh::Joblist::each())) {
 		my $pid      = $job->{pid};
 		my $command  = $job->{call};
-	    
-		$result .= "[$visindex] $pid $command";
-
-		if ($job->{running}) { $result .= "\n"; }
-		else                 { $result .= ' ('.Psh::Locale::get_text('stopped').")\n"; }
 		$visindex++;
+
+		next if $opt->{'r'} and !$job->{running};
+		next if $opt->{'s'} and $job->{running};
+
+	    if ($opt->{'p'}) { # print pid's only
+			$result.= "$pid\n";
+		} else {
+			$result .= "[$visindex] $pid $command";
+
+			if ($job->{running}) { $result .= "\n"; }
+			else                 { $result .= ' ('.Psh::Locale::get_text('stopped').")\n"; }
+		}
 	}
 
 	if (!$result) {
